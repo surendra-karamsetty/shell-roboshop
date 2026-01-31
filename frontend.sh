5b55262084f5a6d588bc3f002354344d0caa0762
surendra-karamsetty <surendra3207@gmail.com>
2026-01-31 18:05:39 +0530

    Commit Changes

--- frontend.sh
+++ frontend.sh
@@ -20,36 +20,36 @@ VALIDATION(){
 }
 
 
-dnf module disable nginx -y
+dnf module disable nginx -y &>>$LOG_FILE
 VALIDATION $? "disable nginx"
 
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
 VALIDATION $? "module enable nginx:1.24"
 
-dnf install nginx -y
+dnf install nginx -y &>>$LOG_FILE
 VALIDATION $? "nginx Instalation"
 
-systemctl enable nginx 
+systemctl enable nginx &>>$LOG_FILE
 VALIDATION $? "enable nginx"
 
-systemctl start nginx 
+systemctl start nginx &>>$LOG_FILE
 VALIDATION $? "nginx Started"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
 VALIDATION $? "Remove the default content"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 VALIDATION $? "Dowload the code"
 
-cd /usr/share/nginx/html 
+cd /usr/share/nginx/html &>>$LOG_FILE
 VALIDATION $? "Going to html path"
 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATION $? "Unzip the code"
 
-cp /$SCRIPT_PATH/nginx.conf /etc/nginx/nginx.conf
+cp /$SCRIPT_PATH/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
 VALIDATION $? "nginx.conf file copied"
 
-systemctl restart nginx 
+systemctl restart nginx &>>$LOG_FILE
 VALIDATION $? "Restarted nginx server"
 
