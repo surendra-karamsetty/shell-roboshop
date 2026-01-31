9fdf1c7d971ceee43d8f4764ec0a9a12b9a754f7
surendra-karamsetty <surendra3207@gmail.com>
2026-01-31 12:39:09 +0530

    Commit Changes

--- instance.sh
+++ instance.sh
@@ -9,18 +9,18 @@ for instance in $@
 do
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
-    --instance-type t3.micro \
+    --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
 	--query 'Instances[0].InstanceId' \
     --output text )
 
-    if [ $instance == 'frontend' ]; then
-        ip=$(
+    if [ $instance == "frontend" ]; then
+        IP=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
-            --output text 
+            --output text
         )
         RECORD_NAME="$DOMINE_NAME" #venkata.online
     else
